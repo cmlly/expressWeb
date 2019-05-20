bc841354bc1f7dd896d23a0f482208a55b36cdfa
cmlly <940767533@qq.com>
2019-05-20 09:37:09 +0800

    提交了基本的代码

---
+++ app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
+var homeRouter = require('./routes/home');
+
+mongoose.connect('mongodb://localhost:27017/blog');
+var db = mongoose.connection;
+db.on('open',function(){
+  console.log("successful")
+});
+db.on('error',function(){
+  console.log("failed")
+})
+
+var app = express();
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/login',loginRouter); // 即为为路径 /login 设置路由
+app.use('/register',registerRouter); // 即为为路径 /register 设置路由
+app.use('/home',homeRouter); // 即为为路径 /home 设置路由
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
